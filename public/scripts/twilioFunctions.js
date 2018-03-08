d697420ab1e1c9e6b35a70a14b52aad3cde81445
Dylanlj <dylanl-j@hotmail.ca>
2018-03-08 21:44:14 +0000

    set up basics of twilio

---
+++ public/scripts/twilioFunctions.js
@@ -0,0 +1,47 @@
+
+
+
+
+var accountSid = 'AC76eb6ee8590a47c08cd0564696b08a95'; // Your Account SID from www.twilio.com/console
+
+var authToken = require('./confidential.js').twilioToken;   // Your Auth Token from www.twilio.com/console
+
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+ let twilio = () => {
+
+//the two user functions can be turned into one
+
+  let notifications = {
+  // to user
+    notifyOrderConfirmed: function (){
+      client.messages.create({
+          body: `Hi , your order for orderlist has been received and will be ready for pickup in minutes, here's the tracking link`,
+          to: '+14164522009',  // Text this users number
+          from: '+16476997847' // From a valid Twilio number
+      })
+      .then((message) => console.log(message.sid));
+    },
+
+    // to user
+    notifyOrderDone: function () {
+      client.messages.create({
+          body: `Hi user, your order of orderlist is ready for pickup`,
+          to: '+14164522009',  // Text this users number
+          from: '+16476997847' // From a valid Twilio number
+      })
+      .then((message) => console.log(message.sid));
+    },
+    //to restaurant
+    notifyRestaurant: function(){
+      client.messages.create({
+          body: `Hi restaurant, an order for orderlist has been placed by user, respond with the time in it will take in minutes, or here link, or type cancel to cancel the order`,
+          to: '+14164522009',  // Text this restaurant number
+          from: '+16476997847' // From a valid Twilio number
+      })
+      .then((message) => console.log(message.sid));
+    }
+  }
+}
+module.exports = twilio
