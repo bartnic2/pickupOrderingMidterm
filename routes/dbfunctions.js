6a3e0c3e40881ddf54df2cd28c82c2263530c27c
Piotr <bartnic1@gmail.com>
2018-03-09 01:10:05 +0000

    Added a few new functions to dbfunctions.js, and made resolve and reject aspects make more sense

--- routes/dbfunctions.js
+++ routes/dbfunctions.js
@@ -3,69 +3,84 @@ var knex = require('knex')(require('../knexfile.js').development)
 module.exports = {
   getAllRestaurantItems: function(restaurant_name){
     return new Promise(function(resolve, reject){
-      resolve(knex.select('items.id', 'items.name', 'category', 'price', 'items.images', 'size', 'items.description').from('items').join('restaurant', 'restaurant_id', 'restaurant.id').where('restaurant.name','=',restaurant_name)
+      knex.select('items.id', 'items.name', 'category', 'price', 'items.images', 'size', 'items.description').from('items').join('restaurant', 'restaurant_id', 'restaurant.id').where('restaurant.name','=',restaurant_name)
         .then(function(rows){
-          return rows;
-        }))
-        .catch(function(err){
-          console.log(err);
+          return resolve(rows);
         })
-      reject(function(err){
-          return err;
-      })
+        .catch(function(err){
+          return reject(err);
+        })
     })
   },
   getRestaurantData: function(restaurant_name){
     return new Promise(function(resolve, reject){
-      resolve(knex.select('name', 'address', 'email_address', 'phone_number', 'description', 'images').from('restaurant').where('restaurant.name','=',restaurant_name)
+      knex.select('name', 'address', 'email_address', 'phone_number', 'description', 'images').from('restaurant').where('restaurant.name','=',restaurant_name)
         .then(function(rows){
-          return rows;
-        }))
-        .catch(function(err){
-          console.log(err);
+          return resolve(rows);
         })
-      reject(function(err){
-          return err;
-      })
+        .catch(function(err){
+          return reject(err);
+        })
     })
   },
   getRestaurantLoginData: function(restaurant_name){
     return new Promise(function(resolve, reject){
-      resolve(knex.select('email_address', 'password').from('restaurant').where('restaurant.name','=',restaurant_name)
+      knex.select('email_address', 'password').from('restaurant').where('restaurant.name','=',restaurant_name)
         .then(function(rows){
-          return rows;
-        }))
-        .catch(function(err){
-          console.log(err);
+          return resolve(rows);
         })
-      reject(function(err){
-          return err;
-      })
+        .catch(function(err){
+          return reject(err);
+        })
     })
   },
   getAllCustomerData: function(customer_name){
     return new Promise(function(resolve, reject){
-      resolve(knex.select('name', 'address', 'email_address', 'password', 'phone_number').from('customer').where('customer.name','=',customer_name)
+      knex.select('name', 'address', 'email_address', 'password', 'phone_number').from('customer').where('customer.name','=',customer_name)
         .then(function(rows){
-          return rows;
-        }))
-        .catch(function(err){
-          console.log(err);
+          return resolve(rows);
         })
-      reject(function(err){
-          return err;
-      })
+        .catch(function(err){
+          return reject(err);
+        })
     })
   },
   getAllItems: function(){
     return new Promise(function(resolve, reject){
-      resolve(knex.select().from('items')
+      knex.select().from('items')
         .then(function(rows){
-          return rows;
-        }))
-      reject(function(err){
-          return err;
-      })
+          return resolve(rows);
+        })
+        .catch(function(err){
+          return reject(err);
+        })
+    })
+  },
+  setRestaurantItems: function(primaryKey, newItemsObj){
+    return new Promise(function(resolve, reject){
+      for (item in newItemsObj){
+        knex.from('items').where('items.id', '=', primaryKey).update(item,newItemsObj[item]).catch(function(err){
+          return reject(err);
+        })
+      }
+      return resolve("success");
+    })
+  },
+  checkForCustomer: function(customerName){
+    return new Promise(function(resolve, reject){
+      knex.select().from('customer').where('name','=',customerName)
+        .then(function(rows){
+          if(rows.length === 0){
+            return resolve(false);
+          }else{
+            return resolve(true);
+          }
+        })
+        .catch(function(err){
+          return reject(err);
+        })
     })
   }
 }
+
+//Create a function where the restaurant can set all its 'item' data
--- routes/dbtest.js
+++ routes/dbtest.js
@@ -1,8 +1,8 @@
 //When importing a file, you have to use ./
 dbfunctions = require("./dbfunctions.js");
 
-//Function 1:
-// dbfunctions.getAllRestaurantItems("John Wayne")
+//Function 1: Get all restaurant items data
+// dbfunctions.getAllRestaurantItems("Steak and Fries")
 // .then(function(rows){
 //   console.log(rows);
 // })
@@ -10,7 +10,7 @@ dbfunctions = require("./dbfunctions.js");
 //   console.log(err);
 // });
 
-//Function 2:
+//Function 2: Get restaurant data (name, address, image locations)
 // dbfunctions.getRestaurantData("Steak and Fries")
 // .then(function(rows){
 //   console.log(rows);
@@ -19,7 +19,7 @@ dbfunctions = require("./dbfunctions.js");
 //   console.log(err);
 // });
 
-//Function 3:
+//Function 3: Get restaurant login data: e-mail and password
 // dbfunctions.getRestaurantLoginData("Steak and Fries")
 // .then(function(rows){
 //   console.log(rows);
@@ -28,8 +28,8 @@ dbfunctions = require("./dbfunctions.js");
 //   console.log(err);
 // });
 
-//Function 4:
-// dbfunctions.getAllCustomerData("Steak and Fries")
+//Function 4: Get all customer data
+// dbfunctions.getAllCustomerData("John Wayne")
 // .then(function(rows){
 //   console.log(rows);
 // })
@@ -37,11 +37,33 @@ dbfunctions = require("./dbfunctions.js");
 //   console.log(err);
 // });
 
-//Function 5:
-// dbfunctions.getAllItems("Steak and Fries")
+// Function 5: Get all items for all restaurants
+// dbfunctions.getAllItems()
 // .then(function(rows){
 //   console.log(rows);
 // })
 // .catch(function(err){
 //   console.log(err);
 // });
+
+//Function 6: Check for specific customer
+// dbfunctions.checkForCustomer("John Wayne")
+// .then(function(response){
+//   console.log(response);
+// })
+// .catch(function(err){
+//   console.log(err);
+// })
+
+//Function 7: Set restaurant items
+// Note - I don't know what happens if an incorrect key is entered. I think it might create it that key...
+
+// dbfunctions.setRestaurantItems(1, {price: "7.00"})
+// .then(function(response){
+//   console.log(response);
+// })
+// .catch(function(err){
+//   console.log(err);
+// })
+
+//Function 8: Store orders
